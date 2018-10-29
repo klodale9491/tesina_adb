31f84de20b91d4e6602e0f1ccfba1af25f6b7243
alessio <klodale9491@gmail.com>
2018-10-29 08:15:08 +0100

    classificatore ottimizzato, rinominati file.

--- sources/myknnclassify.m
+++ sources/myknnclassify.m
@@ -2,37 +2,13 @@ function [tst_cls] = myknnclassify(tst_idx,dists,classes,K)
     % per ognuno degli elementi del test set calcolo le distanze
     % con gli elementi preclassificati del training set.
     tst_cls = strings(1,length(tst_idx));
-    classes = cellstr(classes);
-    for i=1:length(tst_idx)
-        % mappa associativa delle occorrenze, classes(cellArray)
-        occ_map = containers.Map(classes,zeros(1,length(classes)));
-        
+    for i=1:length(tst_idx)        
         % fra le distanze pre-computate prendo le prime k
-        % e verifico dove stanno la maggiornaza dei vicini.
-        curr_dist = dists(tst_idx(:,i),:);
-        [out,ind] = sort(curr_dist);
-        for j=1:K
-            near_elem_ind = ind(j);
-            near_elem_cls = classes{near_elem_ind}; % classe dell'elemento più vicino
-            occ_map(near_elem_cls) = occ_map(near_elem_cls) + 1; % aumento il numero di vicini per la classe near_elem_cls
-        end
-        [cls,val] = max_map(occ_map);
-        tst_cls(i) = cls;
+        % e verifico in quale classe stanno la maggiornaza dei vicini.
+        curr_dist = dists(tst_idx(i),:);
+        [~,ind] = sort(curr_dist);
+        bst_k_cls = classes(ind(1,1:K)); % le classi dei primi K vicini
+        tst_cls(i) = mode(bst_k_cls); % torna la classe con il maggiorn numero di frequenze(la moda)
     end
-    
-    % funzione che calcola il massimo e torna chiave e valore di questo
-    function [max_key,max_val] = max_map(map)
-        max_val = -1;
-        max_key = "";
-        keys = map.keys();
-        vals = map.values();
-        for k=1:length(keys)
-           if vals{k} > max_val
-               max_val = vals{k};
-               max_key = keys{k};
-           end
-        end
-    end
-
 end
 
--- sources/run_classificator.m
+++ sources/run_classificator.m
@@ -1,33 +1,54 @@
-clc
-clear all
+clc;
+clear all;
 
-load('C:\Users\alessio\Documents\MATLAB\tesina_adb\distances\euclidean\euclidean_k_2.mat'); % carico le distanze 'dists'
+load('C:\Users\alessio\Documents\MATLAB\tesina_adb\distances\sw\sw.mat'); % carico le distanze 'dists'
 load('C:\Users\alessio\Documents\MATLAB\tesina_adb\datasets\classes.mat'); % carico le classi 'classes'
 
-% 10-Fold cross validation
+% inizializzazione variabili modello di classificazione.
 n = length(dists);
-kfolds = 10;
-kneigh = 7;
 unq_cls = unique(classes); % in tutto vi saranno 6 classi uniche.
 all_indices = linspace(1,n,n);
 
-% kfolds test di classificazione
-err = zeros(1,kfolds); % percentuale di errore del classificatore, per ognuna delle iterazioni
-perc = 0.9;
-for i=1:kfolds
-    part = cvpartition(n,'HoldOut',1 - perc);
-    test_indices  = all_indices(part.test); % vettore LOGICAL : il test e' 1/kfolds di tutto il dataset
-    train_indices = all_indices(part.training);% vettore LOGICAL : il train e' (kfolds - 1) / kfolds di tutto il dataset
-    % dato he le classi sono 7, selezionando i primi 7 vicino una classe
-    % avrà la maggioranza di vicini(almeno 2)
-    tst_cls = myknnclassify(test_indices,dists,classes,kneigh);
+% 9 livelli di perc di training set, 7 livelli di vicini.
+%{
+data = zeros(9,7); 
+for kneigh=1:7 % proviamo su un differente numero di vicini
+    for j=1:9 
+        perc = 0.1 * j; % proviamo su differenti percentuali di training set
+
+        % suddivisione del dataset
+        part = cvpartition(n,'HoldOut',1 - perc);
+        test_indices  = all_indices(part.test);% vettore LOGICAL : il test e' 1/kfolds di tutto il dataset
+        train_indices = all_indices(part.training);% vettore LOGICAL : il train e' (kfolds - 1) / kfolds di tutto il dataset
+
+        % confronto fra le classi corrette e calcolate dal classificatore.
+        test_cls = myknnclassify(test_indices,dists,classes,kneigh);
+        real_cls = string(classes(test_indices));
+
+        % verifico quante classificazioni ha sbagliato il mio classificatore
+        conf_mat = confusionmat(test_cls,real_cls);
+        err = 1 - sum(diag(conf_mat))/sum(sum(conf_mat));
+        data(j,kneigh) = err;
+    end 
+end
+%}
+
+% validazione del modello con cross-validation
+kfolds = 10;
+kneigh = 7;
+cross_indices = crossvalind('KFold',all_indices,kfolds);
+err_vec = zeros(1,kfolds);
+
+for k=1:kfolds
+    test_indices  = all_indices(cross_indices == k);% vettore LOGICAL : il test e' 1/kfolds di tutto il dataset
+    train_indices = all_indices(~test_indices);% vettore LOGICAL : il train e' (kfolds - 1) / kfolds di tutto il dataset
+
+    % confronto fra le classi corrette e predette dal classificatore.
+    pred_cls = myknnclassify(test_indices,dists,classes,kneigh);
     real_cls = string(classes(test_indices));
+
     % verifico quante classificazioni ha sbagliato il mio classificatore
-    num_err = 0;
-    for j=1:length(tst_cls)
-        if(strcmp(tst_cls(j),real_cls(j)) == 0)
-            num_err = num_err + 1;
-        end
-    end
-    err(i) = num_err / length(tst_cls);
+    conf_mat = confusionmat(real_cls,pred_cls);
+    err = 1 - sum(diag(conf_mat))/sum(sum(conf_mat));
+    err_vec(1,k) = err;
 end
