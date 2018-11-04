eecf0485fffa88c1fcf1ca551a8c0caef07cd5a3
Alessio <alessio.giorgianni.1991@gmail.com>
2018-11-04 14:36:52 +0100

    nuovo calcolo distanze combinate ottimizzato da rifinire

--- sources/dist_cmb_old.m
+++ sources/dist_cmb_old.m
@@ -60,8 +60,7 @@ M = 4; % numero di misure utilizzate.
 % Il calcolo delle distanze combinate dipende dalla scelta del train/test
 % set, quindi selezioniamo gli elementi di tali insiemi...
 perc = 0.7; % percentuale di training set
-% part = cvpartition(N,'HoldOut',1 - perc);
-part = cvpartition(classes,'HoldOut',1 - perc);
+part = cvpartition(N,'HoldOut',1 - perc);
 all_ind_pos = linspace(1,N,N);
 trn_ind = part.training;
 trn_ind_pos = all_ind_pos(trn_ind);
@@ -73,54 +72,49 @@ cmb_dists = zeros(sum(tst_ind),sum(trn_ind)); % matrice delle distanze combinate
 
 % 1) sx_gen_means (numero_elementi_test_set X numero_misure) : per ognuna delle misure registra la media delle
 % distanze di ognuno degli elementi del test-set verso gli elementi del
+% train set.
+sx_gen_means = zeros(num_tst,M);
+for j=1:M
+    sx_gen_means(:,j) = mean(all_dists(tst_ind,trn_ind,j),2);
+end
 
 % 2) sx_cls_means (numero_elementi_test_set X num_classi_train_set X numero_misure)
 % mi interessa memorizzare per ognuno degli elementi del test-set la sua
 % distanza media dalle classi per ognuna delle misure.
 unq_cls_trn = unique(classes(trn_ind));
 num_cls_trn  = length(unq_cls_trn);
-sx_gen_means = zeros(N,N,M);
-sx_cls_means = zeros(N,N,M);
+sx_cls_means = zeros(num_cls_trn,num_tst,M);
+sx_cls_means2 = zeros(num_tst,num_trn,M);
 % mi interessa sapere quali sono gli indici degli elementi del train-set 
 % per ognuna delle classi. 
 % trn_cls_elm_own : memorizza per ogni elemento del train-set la sua classe di appartenenza 
 % fra le num_trn_cls disponibili.
+cls_elm_own = logical(zeros(num_trn,num_cls_trn));
 elm_per_cls = zeros(num_cls_trn,1); % numero elementi per ognuna delle classi del train set
-log_pos_mat = logical(zeros(N,num_cls_trn));
-
-% calcolo della matrice di medie generiche.
-for j=1:M
-    sx_gen_means(tst_ind,trn_ind,j) = mean(all_dists(tst_ind,trn_ind,j),2) * ones(1,sum(trn_ind));
-end
-
-for c=1:num_cls_trn % per ognuna delle misure
-    log_pos_mat(:,c) = strcmp(classes,unq_cls_trn(c)) & trn_ind;
+log_pos_mat = logical(zeros(sum(trn_ind),num_cls_trn));
+for c=1:num_cls_trn
+    log_pos_mat(:,c) = not(cellfun('isempty', strfind(classes(trn_ind), unq_cls_trn(c)))); % posizioni logiche 
+    ind_pos = find(log_pos_mat(:,c)); % posizione numeriche 
+    cls_elm_own(ind_pos,c) = 1; % indici delle classe-iesima nel train set
     elm_per_cls(c) = sum(log_pos_mat(:,c));
-    for j=1:M % per ognuna delle classi
+end
+for j=1:M % per ognuna delle misure
+    for c=1:num_cls_trn % per ognuna delle classi
+        trn_cls_ind = trn_ind_pos(cls_elm_own(:,c));% indici del train della classe corrente c-sima
         % colonna di distanze medie di ogni elemento del test 
         % con gli elementi della classe c utilizzando la misura j
-        sx_cls_means(tst_ind,log_pos_mat(:,c),j) = mean(all_dists(tst_ind,log_pos_mat(:,c),j),2) * ones(1,elm_per_cls(c));
+        sx_cls_means(c,:,j) = mean(all_dists(tst_ind_pos,trn_cls_ind,j),2);
+        sx_cls_means2(:,log_pos_mat(:,c),j) = repmat(sx_cls_means(c,:,j)',1,sum(log_pos_mat(:,c))); % spiaccico la colonna sugli indici di log_pos
     end
 end
 
 % 3) calcolo del vettore dei pesi per ognuna delle misure utilizzate.
-W = zeros(N,M); % matrice di pesi test/misure.
-diff_sq1 = (sx_cls_means - sx_gen_means).^2;
-diff_sq2 = (all_dists - sx_cls_means).^2;
-
+W = zeros(1,M);
 for j=1:M
-    sums_1 = zeros(N,M);
-    sums_2 = zeros(N,M);
-    for c=1:num_cls_trn
-        one_ind = find(log_pos_mat(:,c) == 1);
-        sums_1(:,j) = sums_1(:,j) + (elm_per_cls(c) * diff_sq1(:,one_ind(1),j));
-        sums_2(:,j) = sums_2(:,j) + sum(diff_sq2(:,one_ind(1),j));
-    end
-    sums_1 = sums_1 / (num_cls_trn - 1);
-    sums_2 = sums_2 / (num_trn - num_cls_trn); 
+    btw_grp_var = dist_cmb_bgv(trn_ind,tst_ind,sx_gen_means,sx_cls_means,elm_per_cls,j);
+    wht_grp_var = dist_cmb_wgv(trn_ind,tst_ind,sx_cls_means2,all_dists,elm_per_cls,j);
+    W(j) = btw_grp_var/wht_grp_var;
 end
-
-W = sums_1 ./ sums_2;
 
 % 4) calcolo delle distanze combinate finali (da sistemare utilizzando il reshape(), piu' veloce.)
 for i=1:num_tst
--- sources/dist_cmb_wgv.m
+++ sources/dist_cmb_wgv.m
@@ -1,7 +1,5 @@
 function [wgv] = cmb_wgv(trn_ind,tst_ind,sx_cls_means,all_dists,elm_per_cls,j)
     % CMB_WGV Within Group Variability
-    num_elm_trn = sum(trn_ind);
-    num_cls_trn = length(elm_per_cls);
-    wgv = sum(sum(all_dists(tst_ind,trn_ind,j) - sx_cls_means(:,:,j)).^2) / (num_elm_trn - num_cls_trn);
+    
 end
 
